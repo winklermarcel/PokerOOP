fdf28317eebbdef32104fafed84dd0d9756bd95f
PregOfficial <thomaspregenzer@gmail.com>
2015-11-29 21:30:45 +0100

    MeinCommit

--- src/com/htlinn/pokeroop/winkler_pregenzer/Poker.java
+++ src/com/htlinn/pokeroop/winkler_pregenzer/Poker.java
@@ -1,7 +1,7 @@
 package com.htlinn.pokeroop.winkler_pregenzer;
 
 import java.util.Random;
-//By Thomas & Marcel
+//By Thomas Pregenzer
 public class Poker {
 
 	public static void main(String[] args) {
